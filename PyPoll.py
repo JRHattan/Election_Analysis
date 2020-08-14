818fa0f3eda6743e4e118c4010a4638c706145d5
JRHattan <68794172+JRHattan@users.noreply.github.com>
2020-08-14 11:22:47 -0500

    Update PyPoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -6,14 +6,16 @@ import os
 file_to_load = os.path.join("Resources", "election_results.csv")
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-#election_data = open(file_to_load, "r")
+election_data = open(file_to_load, "r")
 
 with open(file_to_load) as election_data:
 
 # Create a filename variable to a direct or indirect path to the file.
 # Using the open() function with the "w" mode we will write data to the file.
-    open(file_to_save, "w")
+    #open(file_to_save, "w")
 
+    # Print the file object.
+    #print(election_data)
 
 #Write down the names of all the candidates.
     file_reader = csv.reader(election_data)
@@ -31,13 +33,14 @@ with open(file_to_load) as election_data:
 #Get the total votes cast for the election.
 
 # Close the file.
-election_data.close()
+
 
 
 # Create a filename variable to a direct or indirect path to the file.
 
 
 # Using the with statement open the file as a text file.
+
 
 
 
