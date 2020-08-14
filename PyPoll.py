46837cca67452b2924881edb3f492b439649d76f
JRHattan <68794172+JRHattan@users.noreply.github.com>
2020-08-14 13:19:49 -0500

    Update PyPoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -6,34 +6,51 @@ import os
 file_to_load = os.path.join("Resources", "election_results.csv")
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
-election_data = open(file_to_load, "r")
+total_votes = 0
+candidate_options = []
+candidate_votes = {}
+
+
+#election_data = open(file_to_load, "r")
 
 with open(file_to_load) as election_data:
 
 # Create a filename variable to a direct or indirect path to the file.
 # Using the open() function with the "w" mode we will write data to the file.
+    file_reader = csv.reader(election_data)
+    headers = next(file_reader)
+
     #open(file_to_save, "w")
 
     # Print the file object.
     #print(election_data)
-
 #Write down the names of all the candidates.
-    file_reader = csv.reader(election_data)
-
+    # Print the candidate list.
+        
     # Read and print the header row.
-    headers = next(file_reader)
-    print(headers)
-
+    #print(headers)
     # Print each row in the CSV file.
     for row in file_reader:
-        print(row)
-#Add a vote count for each candidate.
+        total_votes =+ 1
 
+        candidate_name = row[2]
+
+        if candidate_name not in candidate_options:
+            # Add it to the list of candidates.
+            candidate_options.append(candidate_name)
+            print(candidate_options)
+#Add a vote count for each candidate.
+           # 2. Begin tracking that candidate's vote count.
+            candidate_votes[candidate_name] = 0
+
+
+        candidate_votes[candidate_name] += 1
 
 #Get the total votes for each candidate.
 
 
 #Get the total votes cast for the election.
+    print(candidate_votes)
 
 # Close the file.
 
